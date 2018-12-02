03c115f7eb9938314a3cc2a0928f6e88c7bbf75c
sdcwlfx <18463102717@163.com>
2018-12-02 20:10:40 +0800

    完成e1

---
+++ src/sc/ustc/controller/SimpleController.java
@@ -0,0 +1,55 @@
+package sc.ustc.controller;
+
+import java.io.IOException;
+//import java.io.PrintWriter;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleController
+ */
+@WebServlet("/SimpleController")
+public class SimpleController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SimpleController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	    doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		out.println("<html><head><title>SimpleController</title></head><body>ª∂”≠ π”√SimpleController!</body></html>");
+//		try {
+//			request.getRequestDispatcher("/welcome.html").forward(request,response);
+//			return;
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+		
+	}
+
+}
